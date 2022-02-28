d1dcf6abe8e149e827b5b07b36a948b90204b3f1
Jun <junyuplee@mines.edu>
2022-02-27 23:42:51 -0700

    <Created TestBoard class>

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,24 @@
 package experiment;
+import java.util.*;
 
 public class TestBoard {
-
+	
+	private Set<TestBoardCell> nullCells;
+	private TestBoardCell nullCell;
+	
+	public TestBoard() {
+		
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		
+	}
+	
+	public Set<TestBoardCell> getTargets() {
+		return nullCells;
+	}
+	
+	TestBoardCell getCell( int row, int col ) {
+		return nullCell;
+	}
 }
