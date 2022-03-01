5ea68dbad8c65ab3d2250d3e7a622b1c2448d5ae
Jun <junyuplee@mines.edu>
2022-03-01 15:09:01 -0700

    <Added pseudocode>

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -17,11 +17,29 @@ public class TestBoard {
 	}
 	
 	public void calcTargets(TestBoardCell startCell, int pathlength) {
-		
+		// base case
+		// visited list is used to avoid backtracking.  Set to empty list
+		// targets will ultimately contain the list of cells. Set to an empty list.
+		// Add the start location to the visited list (so no cycle through this cell)
+		//Call recursive function - name findAllTargets
+			// startCell and pathLength as parameters
 	}
 	
+	/*
+	 * findALlTargets psuedo
+	 * param: thisCell, numSteps
+	 * 
+	 * for each adjCell in adjacentCells
+	 * if already visited, skip
+	 * 
+	 * add adjCell to visited list
+	 * if numSteps==1, add adjCell to Targets
+	 * else call findAllTargets with adjCell, numSteps-1
+	 * remove adjCell from visited List
+	 */
+	
 	public Set<TestBoardCell> getTargets() {
-		return new HashSet<TestBoardCell>();
+		return targets;
 	}
 	
 	public TestBoardCell getCell( int col, int row ) {
