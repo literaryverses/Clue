328209cfa9e21e6c4b278f1455a4fea20a6e2b76
Matthew Brause <matthewbrause@mines.edu>
2022-03-04 18:34:05 -0700

    read in csv

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -2,26 +2,43 @@
 // Matthew Brause
 
 package clueGame;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.*;
 
 public class Board {
 	private BoardCell[][] grid;
 	private Set<BoardCell> targets;
 	private Set<BoardCell> visited;
-	final static int COLS = 4;
-	final static int ROWS = 4;
+	final static int COLS = 21;
+	final static int ROWS = 25;
+	String[][] cellTags;
 	
-	public Board() {
-		visited = new HashSet<BoardCell>();
-		targets = new HashSet<BoardCell>();
-		grid = new BoardCell[COLS][ROWS];
-		for (int i =0; i < COLS; i++) {
-			for (int j=0; j < ROWS; j++) {
-				grid[i][j] = new BoardCell(i,j);
-			}
-		}
-		makeAdjs();
-	}
+	private static Board theInstance = new Board();
+    // constructor is private to ensure only one can be created
+    private Board() {
+        super();
+        cellTags = new String[ROWS][COLS];
+        visited = new HashSet<BoardCell>();
+   		targets = new HashSet<BoardCell>();
+   		grid = new BoardCell[ROWS][COLS];
+   		for (int i =0; i < ROWS; i++) {
+   			for (int j=0; j < COLS; j++) {
+   				grid[i][j] = new BoardCell(i,j);
+   			}
+   		}
+   		makeAdjs();
+    }
+    // this method returns the only Board
+    public static Board getInstance() {
+           return theInstance;
+    }
+    /*
+     * initialize the board (since we are using singleton pattern)
+     */
+    public void initialize()
+    {
+    }
 	
     public void calcTargets(BoardCell startCell, int pathLength) {
         this.visited.add(startCell);
@@ -45,21 +62,21 @@ public class Board {
 		return targets;
 	}
 	
-	public void setAdjList( int col, int row ) {
-		BoardCell newCell = grid[col][row];
+	public void setAdjList( int row, int col ) {
+		BoardCell newCell = grid[row][col];
 		
 		//adds cells to the adjacent cell list if they are in the boundaries
-		if (row-1 >= 0) {
-			newCell.addAdjList(grid[col][row-1]);
-		} 
-		if (row+1 < ROWS) {
-			newCell.addAdjList(grid[col][row+1]);
-		} 
 		if (col-1 >= 0) {
-			newCell.addAdjList(grid[col-1][row]);
+			newCell.addAdjList(grid[row][col-1]);
 		} 
 		if (col+1 < COLS) {
-			newCell.addAdjList(grid[col+1][row]);
+			newCell.addAdjList(grid[row][col+1]);
+		} 
+		if (row-1 >= 0) {
+			newCell.addAdjList(grid[row-1][col]);
+		} 
+		if (row+1 < ROWS) {
+			newCell.addAdjList(grid[row+1][col]);
 		} 
 		 
 	}
@@ -67,16 +84,40 @@ public class Board {
 	public void makeAdjs() {
 		//creates the adjacent cell list for all the cells in the grid
 		
-		for (int i = 0; i < COLS; i++) {
-			for (int j = 0; j < ROWS; j++) {
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
 				setAdjList(i,j);
 			}
 		}
 	}
 	
-	public BoardCell getCell( int col, int row ) {
-		BoardCell cell = grid[col][row];
+	public BoardCell getCell( int row, int col ) {
+		BoardCell cell = grid[row][col];
 
 		return cell;
 	}
+	
+public void loadDataFile(String fileName) throws FileNotFoundException {
+		String[] lineTags;
+		String line;
+		int count = 0;
+		// open file for reading
+		FileReader fileReader = new FileReader(fileName);
+		Scanner scan = new Scanner(fileReader);
+		
+		// loop over lines and add to array as a Student
+		while (scan.hasNextLine()) {
+			
+			// grab the data as strings
+			line = scan.nextLine();
+			lineTags = line.split(",");
+			for (int i = 0; i < lineTags.length; i++) {
+				cellTags[count][i] = lineTags[i];
+			}
+			count++;
+			
+		}
+
+	};
+	
 }
