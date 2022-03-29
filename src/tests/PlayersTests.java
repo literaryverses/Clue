5f61ff698c14310a484b4d8086f35b5b953eafb7
Matthew Brause <matthewbrause@mines.edu>
2022-03-29 13:37:54 -0600

    Added testing different colors test

--- src/tests/PlayersTests.java
+++ src/tests/PlayersTests.java
@@ -5,15 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PlayersTests {
+import clueGame.Player;
+import clueGame.ComputerPlayer;
+import clueGame.HumanPlayer;
 
+class PlayersTests {
+	
+	private HumanPlayer man;
+	private ComputerPlayer comp;
+	private ComputerPlayer comp2;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		man = new HumanPlayer();
+		comp = new ComputerPlayer();
+		comp2 = new ComputerPlayer();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDifferentColors() {
+		assertTrue(man.getColor() != comp.getColor());
+		assertTrue(man.getColor() != comp2.getColor());
+		assertTrue(comp2.getColor() != comp.getColor());
 	}
 
 }
